2cd707406d37205612aea42c2c8d33a337c52ad9
YashYadav08 <yashyadav008feb@gmail.com>
2021-06-29 19:01:35 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,142 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const _ = require("lodash");
+//var items = [];
+
+const app= express();
+
+app.set("view engine","ejs");
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+
+mongoose.connect("mongodb+srv://admin_yash:yash2702@cluster0.oir2y.mongodb.net/toDoListDB", {useNewUrlParser:true,useUnifiedTopology:true, useFindAndModify:false});
+
+//Schema->model->document (document = rows of sql, model = table, schema is the format)
+const itemsSchema = {   //Schema for our Database
+    name: String
+};
+
+const Item = mongoose.model("Item", itemsSchema);   //model based on the itemsSchema
+
+//making a few documents for the list(documents are same as rows in sql)
+const item1 = new Item({
+    name: "This is Task-1: Go and Sleep"
+});
+const item2 = new Item({
+    name: "Workout time"
+});
+const item3 = new Item({
+    name: "Get back to studying after eating some food."
+});
+
+const defaultItems = [item1, item2, item3];
+
+const listSchema = {
+    name: String,
+    items: [itemsSchema]
+};
+const List = mongoose.model("List", listSchema);
+
+app.get("/", function(req,res)
+{
+    var today = new Date();
+    var options= { weekday:"long", day:"numeric", month:"long"};
+    var day = today.toLocaleDateString("en-US", options);
+
+    Item.find({}, function(err, reslist){
+        if(reslist.length === 0)
+        {
+            Item.insertMany(defaultItems, function(err){
+                if(err){
+                    console.log(err);
+                }else{
+                    console.log("Successfully saved Default-Items to the Database.");
+                }
+            });
+            res.redirect("/");
+        }else{
+            res.render("list", {listTitle:day, newListItems: reslist});
+        }
+    });
+});
+
+//For custom webpages
+app.get("/:customListName", function(req,res){
+    const customListName = _.capitalize(req.params.customListName);
+    
+    List.findOne({name:customListName}, function(err, reslist){
+        if(!err){
+            if(!reslist){
+                const list = new List({
+                    name: customListName,
+                    items: defaultItems
+                });
+                list.save();
+                res.redirect("/"+customListName);
+            }
+            else{
+                res.render("list", {listTitle:reslist.name, newListItems:reslist.items});
+            }
+        }
+    });
+});
+
+app.post("/", function(req,res)
+{
+    const itemName = req.body.newItem;
+    const listName = req.body.Button;
+
+    var today = new Date();
+    var options= { weekday:"long", day:"numeric", month:"long"};
+    var day = today.toLocaleDateString("en-US", options);
+
+    const newitem = new Item({
+        name: itemName
+    });
+
+    if(listName === day){   //for every item added to today's list
+        newitem.save();
+        res.redirect("/");  //This will redirect it to the app.get part
+    }else{  //for items added to custom list
+        List.findOne({name:listName}, function(err, reslist){
+            reslist.items.push(newitem);
+            reslist.save();
+            res.redirect("/"+listName);
+        });
+    }
+   
+});
+
+app.post("/delete", function(req,res){
+    const checkedItemId = req.body.checkbox;
+    const listName = req.body.listTitle;
+
+    var today = new Date();
+    var options= { weekday:"long", day:"numeric", month:"long"};
+    var day = today.toLocaleDateString("en-US", options);
+
+    if(listName === day){
+        Item.findByIdAndRemove(checkedItemId, function(err){
+            if(!err){
+                console.log("Successfully removed the Checked Item!");
+                res.redirect("/");
+            }
+        }); 
+    }else{
+        List.findOneAndUpdate({name:listName}, {$pull:{items:{_id: checkedItemId}}}, function(err, reslist){
+            if(!err){
+                res.redirect("/"+listName);
+            }
+        });
+    }
+    
+});
+
+app.listen(8000, function()
+{
+    console.log("Server is live on port 8000!");
+});
